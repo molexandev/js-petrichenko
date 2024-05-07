938eb21b2d6582689e94f6dc5fb22750912dcf93
molexandev01~ <molexandev@gmail.com>
2024-05-07 16:40:18 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+// a = 15;
+// console.log(a);
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+// console.log(number);
+// console.log(leftBorderWidth);
+
+// {
+//    let result = 50;
+//    console.log(result);
+// }
+
+// let store = 'Hollywood';
+// let storeDescription = {
+//    budget: 10000,
+//    employees: ['Alex', 'Anna', 'Julia'],
+//    products: {
+//       apples: 25,
+//       cucumbers: 50,
+//    },
+//    open: true,
+// };
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам є 18?', '18');
+// console.log(typeof answer);
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('Як вас звати?', '');
+// answers[1] = prompt('Яке ваше прізвище?', '');
+// answers[2] = prompt('Скільки вам років?', '');
+// // document.write(answers);
+// console.log(typeof answers);
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}`);
+const user = 'Ivan';
+alert(`Привіт, ${user}`);
